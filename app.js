a5cb957165945aab80196d83196a8860370382de
bulsaraharshil <bulsaraharshil@gmail.com>
2021-01-02 19:51:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var btn = document.querySelector("#btn")
+var bamt = document.querySelector("#bamt")
+var camt = document.querySelector("#camt")
+var div = document.querySelector("#div")
+var div2 = document.querySelector("#div2")
+var nofnotes = document.getElementsByClassName("noOfNotes");
+
+let notearray = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+
+function cashHandler() {
+    var bill = bamt.value
+    var cash = camt.value
+    var sum = parseInt(cash - bill)
+    if (parseInt(cash) < parseInt(bill)) {
+        var errorMsg = "Cash given amount is entered less than bill amount!!!"
+        div.innerHTML = errorMsg
+    } else {
+        var successMsg = "Amount to be returned: " + sum
+        div.innerHTML = successMsg
+    }
+    let counter = 0;
+    for (i = 0; i < notearray.length; i++) {
+
+        if (sum / notearray[i] >= 1) {
+            counter = parseInt(sum / notearray[i]);
+            sum = sum - notearray[i] * counter;
+            nofnotes[i].innerHTML = `${counter}`
+            if (sum === 0) {
+                break;
+            }
+
+        }
+    }
+}
+btn.addEventListener("click", cashHandler)
